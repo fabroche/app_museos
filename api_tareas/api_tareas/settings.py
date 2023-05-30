509ccebbe315a419b891eca299c6bbd33f01ec4c
fabroche <kraxusmmo@gmail.com>
2023-05-30 02:02:29 -0500

    api sig museo v0.1 lista enrutamiento para swagger

--- api_tareas/api_tareas/settings.py
+++ api_tareas/api_tareas/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
     # my apps
     'task',
+    'museo',
 ]
 
 MIDDLEWARE = [
--- api_tareas/api_tareas/urls.py
+++ api_tareas/api_tareas/urls.py
@@ -1,23 +1,34 @@
-"""
-URL configuration for api_tareas project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+# para Swagger
+from rest_framework.schemas import get_schema_view
+
+# Swagger Docs
+swaggerUi_schema = get_schema_view(
+    title='SwaggerUI ApiMuseo',
+    description='Guide for the API Task',
+    version='v1.0.0',
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/', include('task.urls')),
+    # path('task/', include('task.urls')),
+    path('museo/', include('museo.urls')),
+    path('exposicion/', include('museo.urlsExposiciones')),
+    path('horario/', include('museo.urlsTipoHorario')),
+    path('artista/', include('museo.urlsArtista')),
+    path('estado-conservacion/', include('museo.urlsEstadoConservacion')),
+    path('tecnica/', include('museo.urlsTecnica')),
+
+    path('obra/', include('museo.urlsObras')),
+
+    # End Point para construir el schema de configuracion del Swagger
+    path('schema/', swaggerUi_schema, name="schema-apiTask"),
+
+    # End Point para visitar la Documentacion de la Api
+    path('api-museo/docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url': 'schema-apiTask'}
+    ), name='swagger-ui'),
 ]
